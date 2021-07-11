8e2854a65350f6674ea277404f45d730900ea02f
Kovalyov Dmitriy <kovdmkonst@gmail.com>
2021-07-11 13:07:02 +1000

    add text fields info

--- RGB App/ViewController.swift
+++ RGB App/ViewController.swift
@@ -8,12 +8,52 @@
 import UIKit
 
 class ViewController: UIViewController {
+        
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redColorField: UITextField!
+    @IBOutlet var greenColorField: UITextField!
+    @IBOutlet var blueColorField: UITextField!
+    
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+        colorView.layer.cornerRadius = 15
+        
+
     }
-
-
+    
+    
+    @IBAction func showRedInfo() {
+        let redValue = redSlider.value
+        redColorField.text = "\(round(100 * redValue) / 100)"
+    }
+    
+    @IBAction func showGreenInfo() {
+        let greenValue = greenSlider.value
+        greenColorField.text = "\(round(100 * greenValue) / 100)"
+    }
+    
+    @IBAction func showBlueInfo() {
+        let blueValue = blueSlider.value
+        blueColorField.text = "\(round(100 * blueValue) / 100)"
+    }
+   
+    @IBAction func changeColor() {
+    colorView.backgroundColor = UIColor(
+        red: CGFloat(redSlider.value),
+        green: CGFloat(greenSlider.value),
+        blue: CGFloat(blueSlider.value),
+        alpha: 1
+    )
+    }
 }
+
 
